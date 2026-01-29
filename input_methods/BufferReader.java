8b8bf8f1cd76f468c80e68d4de9f377bccb128d8
victo-silvers <silversvicto@gmail.com>
2026-01-29 05:34:03 +0530

    Added input methods - Scanner , Buffered , Console

--- input_methods/BufferReader.java
+++ input_methods/BufferReader.java
@@ -1,10 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+
+// BufferedReader is faster than scanner because it loads a chunk rather than a character(in scanner) at a time and store that chunk in RAM for faster retrieval
 
 @SuppressWarnings("unused")
 public class BufferReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter your name: ");
+        String name = reader.readLine();
+
+        System.out.println("Enter your age: ");
+        int age = Integer.parseInt(reader.readLine());
+
+        System.out.println("Your name: " + name);
+        System.out.println("Your age: " + age);
+        System.out.println(((Object)age).getClass());
+
         
     }
 }
---
+++ input_methods/Consolemethod.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+
+// Console is designed for reading passwords since it reads passwords without echoing characters to the screen
+public class Consolemethod {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        if (console == null){
+            System.out.println("No console available!");
+            return;
+        }
+
+        String username = console.readLine("Username: ");
+        // Readpassword returns a char[] for security:
+        char[] password = console.readPassword("Password: ");
+        System.out.println("Welcome " + username);
+    }
+}
