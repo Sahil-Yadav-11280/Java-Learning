724798e16229d83e0f878d13acc74b0fa2ac52a9
victo-silvers <silversvicto@gmail.com>
2026-02-05 12:57:43 +0530

    MInor changes in console method for user input

--- input_methods/Consolemethod.java
+++ input_methods/Consolemethod.java
@@ -1,6 +1,7 @@
 import java.io.Console;
 
 // Console is designed for reading passwords since it reads passwords without echoing characters to the screen
+@SuppressWarnings("unused")
 public class Consolemethod {
     public static void main(String[] args) {
         Console console = System.console();
