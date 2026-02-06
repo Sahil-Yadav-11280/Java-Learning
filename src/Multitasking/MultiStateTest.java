e0abf9ac855c46583a4b91481e1ce7e6d9be8702
victo-silvers <silversvicto@gmail.com>
2026-02-06 19:11:32 +0530

    Controlling Timeline with join on many threads

--- src/Multitasking/MultiStateTest.java
+++ src/Multitasking/MultiStateTest.java
@@ -1,4 +1,35 @@
 package Multitasking;
 
 public class MultiStateTest {
+    public static void main(String[] args){
+        Thread t4 = new Thread(() -> {
+           System.out.println("Thread 4: Starting heavy work!!");
+           try{
+               Thread.sleep(3000);
+           } catch(InterruptedException e){
+               System.out.println("Issued found: Thread 4");
+           }
+           System.out.println("Thread 4: Heavy task finished");
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("Thread 2: Waiting for thread 4 to finish its work...");
+            try{
+                t4.join();
+            } catch(InterruptedException e) {
+                System.out.println("Issued found: Thread 2");
+            }
+            System.out.println("Thread 2: Thread 4 has finished task and now I can do my work...");
+        });
+
+        Thread t3 = new Thread(()->{
+           System.out.println("Thread 3: Independent Thread -> free to do its work!");
+        });
+
+        t4.start();
+        t2.start();
+        t3.start();
+
+        System.out.println("Main Thread: Also free to do my work!!");
+    }
 }
